768743b68c5c3eaaeb6cf456f6b02d08903b129b
Poier <kkoossttaa02@gmail.com>
2020-12-14 22:06:08 +0300

    initial

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const path = require('path')
+const csrf = require('csurf')
+const flash = require('connect-flash')
+const Handlebars = require('handlebars')
+const mongoose = require('mongoose')
+const helmet = require('helmet')
+const compression = require('compression')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const authRouters = require('./routes/auth')
+const profileRouters = require('./routes/profile')
+const exphbs = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
+const errorHandler = require('./middleware/error')
+const fileMiddleware = require('./middleware/file')
+const keys = require('./keys')
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  helpers: require('./utils/hbs-helpers'),
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: keys.MONGODB_URI
+
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: keys.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store
+}))
+app.use(fileMiddleware.single('avatar'))
+app.use(csrf())
+app.use(flash())
+app.use(helmet())
+app.use(compression())
+app.use(varMiddleware)
+app.use(userMiddleware)
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRouters)
+app.use('/profile', profileRouters)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000
+
+
+async function start() {
+  try{
+    
+      await mongoose.connect(keys.MONGODB_URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      })
+     
+
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+      })
+  }catch (e) {
+      console.log(e)
+  }
+  
+}
+
+
+
+start()
