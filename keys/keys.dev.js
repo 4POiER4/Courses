3623fdbf4b3f209fa609ad52543529f0f629acb1
Poier <kkoossttaa02@gmail.com>
2020-12-17 00:22:16 +0300

    release v1

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports ={
+    MONGODB_URI: `mongodb+srv://Kosta:nata01011977@cluster0.ijrh5.mongodb.net/shop`,
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '5b9afa38eeba63138638505a562eadcc',
+    EMAIL_FROM: 'kkoossttaa02@gmail.com',
+    BASE_URL: 'http://localhost:3000'
+}
